0077db6e23025021c0dd138c9c8aebcb35cd6297
Revati Pangare <miss.pangarerevati@gmail.com>
2025-06-30 15:01:45 +0530

    Initial commit: AutoPack CLI working version

---
+++ autopack.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+folder=os.listdir('/home/dell/Desktop/autoPack')
+src="/home/dell/Desktop/autoPack"
+catigorys={
+    "Image":[".jpg",".png",".gif"],
+    "Document":[".pdf",".txt",".docx"],
+    "Archive":[".zip",".rar",".7z"],
+    "Music":[".mp3",".wav"],
+    "Videos":[".mp4",".mkv"]
+
+}
+
+for i in folder:
+    root,extention=os.path.splitext(i)
+    for key ,val in catigorys.items():
+         for k in val: 
+              if extention == k:
+                   dest_folder=os.path.join(src,key)
+                   if not os.path.exists(dest_folder):
+                         os.makedirs(dest_folder)
+    
+                   source=os.path.join(src,i)
+                   dest_file=os.path.join(dest_folder,i)
+
+                   shutil.move(source,dest_file)
+                   print(f"Moved {i} --> {key}")
+                   break
+        
+      
+
+
+
+
+
+
